1283cbb510f100d6ff79084eb833cbbf2ddc9521
byronhsu <byronhsu1230@gmail.com>
2020-01-03 20:39:44 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,20 +27,24 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a0a4d88e8039a21fd6c9.js"
+    "url": "webpack-runtime-d787ace7e1d6306a2225.js"
   },
   {
     "url": "commons-30f2c15b6520d58277b1.js"
   },
   {
-    "url": "app-82c7f3f0e0ffe11dd28b.js"
+    "url": "app-1f1358ebe695059dd8bb.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-c5f58849c2f4732d93c1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "dd28447a63f5d079a527af7b8123502b"
+    "revision": "04c87d3d851944f915ccc6b37e4c8834"
+  },
+  {
+    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
+    "revision": "c355c8040c47a63bfb3360e4b7cb6553"
   },
   {
     "url": "manifest.webmanifest",
@@ -132,12 +136,12 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   lastNavigationRequest = event.request.url
 
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^`), ``)
+  pathname = pathname.replace(new RegExp(`^/Wenyan-CTF`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-82c7f3f0e0ffe11dd28b.js`))) {
+  if (!resources || !(await caches.match(`/Wenyan-CTF/app-1f1358ebe695059dd8bb.js`))) {
     return await fetch(event.request)
   }
 
@@ -150,7 +154,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/Wenyan-CTF/offline-plugin-app-shell-fallback/index.html`
   const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
   return await caches.match(offlineShellWithKey)
 })
